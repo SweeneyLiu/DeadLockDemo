05d20b5b930c551cc7bf9fbf8f9ac3e67085da2d
liushuwei <liushuwei@qiyi.com>
2017-08-08 19:20:30 +0800

    Initial commit

---
+++ app/src/main/java/com/lsw/deadlockdemo/DeadLockTest.java
@@ -0,0 +1,16 @@
+package com.lsw.deadlockdemo;
+
+/**
+ * Created by liushuwei on 2017/8/8.
+ */
+
+public class DeadLockTest implements Runnable {
+
+    private int flag;
+
+
+    @Override
+    public void run() {
+
+    }
+}
